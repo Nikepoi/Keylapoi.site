bad523c7db432ee3edd8b5be08b33e748fadaaf6
Nikepoi <rarwzeno@gmail.com>
2025-07-08 16:04:57 +0700

    Update worker.js

--- my-photo-grid-web/js/worker.js
+++ my-photo-grid-web/js/worker.js
@@ -1,28 +1,29 @@
-const GENRES = [ 'asia', 'jav', 'local', 'local_random', 'asia_random', 'barat', 'barat_random' ];
+const GENRES = ['asia', 'jav', 'local', 'local_random', 'asia_random', 'barat', 'barat_random'];
 
-self.onmessage = function (e) { if (e.data === 'start') { let lastVersions = {};
+self.onmessage = function (e) {
+  if (e.data === 'start') {
+    let lastVersions = {};
 
-async function checkUpdate() {
-  try {
-    for (let genre of GENRES) {
-      const response = await fetch(`data/index.${genre}.json`, { cache: 'no-store' });
-      if (!response.ok) continue;
+    async function checkUpdate() {
+      try {
+        for (let genre of GENRES) {
+          const response = await fetch(`data/index.${genre}.json`, { cache: 'no-store' });
+          if (!response.ok) continue;
 
-      const data = await response.json();
-      const currentVersion = data.lastModified;
+          const data = await response.json();
+          const currentVersion = data.lastModified;
 
-      if (lastVersions[genre] && currentVersion !== lastVersions[genre]) {
-        postMessage('update');
+          if (lastVersions[genre] && currentVersion !== lastVersions[genre]) {
+            postMessage('update');
+          }
+
+          lastVersions[genre] = currentVersion;
+        }
+      } catch (err) {
+        console.error('Worker gagal cek update:', err);
       }
-
-      lastVersions[genre] = currentVersion;
     }
-  } catch (err) {
-    console.error('Worker gagal cek update:', err);
+
+    setInterval(checkUpdate, 15000);
   }
-}
-
-setInterval(checkUpdate, 15000);
-
-} };
-
+};
